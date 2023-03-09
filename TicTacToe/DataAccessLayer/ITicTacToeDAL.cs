d5573a53b4305da2ebfb4297a9d4b0a770d78ad8
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-03-09 16:58:34 +0300

    add data access layer

---
+++ TicTacToe/DataAccessLayer/ITicTacToeDAL.cs
@@ -0,0 +1,15 @@
+﻿using TicTacToe.Models;
+
+namespace TicTacToe.DataAccessLayer
+{
+    public interface ITicTacToeDAL
+    {
+        Task<Board> CreateMovementAndRefreshBoard(Movement movement, Board board);
+        Task<Board?> GetBoardByItsId(int boardId);
+        Task<Game?> GetGameByItsBoard(Board board);
+        Task<Player?> GetPlayerByItsId(int playerId);
+        Task<Player> GetSomeComputerPlayer();
+        Task<Game> RefreshGameState(Game game);
+        Task SaveBoard(Board board);
+    }
+}
---
+++ TicTacToe/DataAccessLayer/TicTacToeDAL.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TicTacToe.Data;
+using TicTacToe.Models;
+
+namespace TicTacToe.DataAccessLayer
+{
+    public class TicTacToeDAL : ITicTacToeDAL
+    {
+        private ApplicationDbContext appDbContext;
+
+        public TicTacToeDAL(ApplicationDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<Player?> GetPlayerByItsId(int playerId)
+        {
+            return await appDbContext.Players.FindAsync(playerId);
+        }
+
+        public async Task<Board?> GetBoardByItsId(int boardId)
+        {
+            var boards = await appDbContext.Boards
+                .Where(b => b.Id == boardId)
+                .Include(b => b.PlayerBoards)
+                .ThenInclude(pb => pb.Player)
+                .ToListAsync();
+
+            return boards.FirstOrDefault();
+        }
+
+        public async Task<Game?> GetGameByItsBoard(Board board)
+        {
+            var games = await appDbContext.Games
+                .Where(game => game.ConfiguredBoard.Id == board.Id)
+                .Include(g => g.ConfiguredBoard)
+                .ThenInclude(b => b.Movements)
+                .ToListAsync();
+            var possibleGame = games.FirstOrDefault();
+            // To refresh board status
+            possibleGame?.ConfiguredBoard?.InitializeBoardConfiguration();
+            return possibleGame;
+        }
+
+        public async Task<Game> RefreshGameState(Game game)
+        {
+            // https://docs.microsoft.com/en-us/ef/core/saving/basic#updating-data
+            var entityEntry = appDbContext.Games.Update(game);
+            // TODO: Maybe disable AutoDetectChangesEnabled as it is enabled by default;
+            var stateEntriesWrittenToTheDatabase = await appDbContext.SaveChangesAsync();
+            // TODO: Maybe log stateEntriesWrittenToTheDatabase?
+            return entityEntry.Entity;
+        }
+
+        public async Task<Player> GetSomeComputerPlayer()
+        {
+            return await appDbContext.Players.FirstAsync(p => p.Computer);
+        }
+
+        public async Task SaveBoard(Board board)
+        {
+            foreach (var playerBoard in board.PlayerBoards)
+            {
+                // TODO: Proposed solution: https://stackoverflow.com/a/39165051/3899136
+                var p = await appDbContext.Players.FindAsync(playerBoard.Player.Id);
+                playerBoard.Player = p;
+            }
+            appDbContext.Boards.Add(board);
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Board> CreateMovementAndRefreshBoard(Movement movement, Board board)
+        {
+            if (board.Movements is null)
+                board.Movements = new List<Movement>();
+
+            board.Movements.Add(movement);
+
+            var entityEntryBoard = appDbContext.Boards.Update(board);
+            await appDbContext.SaveChangesAsync();
+
+            return entityEntryBoard.Entity;
+        }
+    }
+}
