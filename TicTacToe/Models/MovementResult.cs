8929e2c3d1168bc2da7175fc9020cf4530f94684
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-03-09 18:15:34 +0300

    add model for movement result

---
+++ TicTacToe/Models/MovementResult.cs
@@ -0,0 +1,20 @@
+﻿namespace TicTacToe.Models
+{
+    public class MovementResult
+    {
+        public bool HasAWinner { get; set; }
+        public bool IsDraw { get; set; }
+        public bool NoMovementsAvailable { get; set; }
+        public bool IsGameOver { get; set; }
+        public Player PlayerWhoMadeLastMove { get; set; }
+
+        public MovementResult(bool winner, bool draw, bool noMovements, bool gameOver, Player lastMovedPlayer)
+        {
+            HasAWinner = winner;
+            IsDraw = draw;
+            NoMovementsAvailable = noMovements;
+            IsGameOver = gameOver;
+            PlayerWhoMadeLastMove = lastMovedPlayer;
+        }
+    }
+}
