4d07b15fab0709184eb0de6f4b6d8cf5b96a8a88
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-03-11 12:37:27 +0300

    add model for one game

---
+++ TicTacToe/Controllers/Models/PlayGame.cs
@@ -0,0 +1,9 @@
+﻿namespace TicTacToe.Controllers.Models
+{
+    public class PlayGame
+    {
+        public int BoardId { get; set; }
+        public int PlayerId { get; set; }
+        public int MovementPosition { get; set; }
+    }
+}
