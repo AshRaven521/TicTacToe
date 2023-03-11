039bd8965fe141671f3eee581c40e66218e4d4e0
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-03-11 12:39:09 +0300

    add model for board creating

---
+++ TicTacToe/Controllers/Models/CreateBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace TicTacToe.Controllers.Models
+{
+    public class CreateBoard
+    {
+        public int FirstPlayerId { get; set; }
+        public int SecondPlayerId { get; set; }
+        public string BoardSize { get; set; } = "3x3";
+    }
+}
